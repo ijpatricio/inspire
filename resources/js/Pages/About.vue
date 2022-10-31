896ae477e5225c3081f11bb3d1cead0847d00b88
Joao Patricio <ijpatricio@gmail.com>
2022-10-31 16:50:18 +0000

    Updates   page

--- resources/js/Pages/About.vue
+++ resources/js/Pages/About.vue
@@ -5,8 +5,15 @@ import GuestLayout from '@/Layouts/GuestLayout.vue'
 <template>
     <GuestLayout>
         <div class="flex flex-col min-h-screen justify-center bg-gray-100">
-            <div class="flex justify-center mt-32">
-
+            <div class="flex justify-center">
+                <div>
+                    <div class="mt-10">
+                        <a href="https://twitter.com/ijpatricio" target="_blank">Twitter: @ijpatricio</a>
+                    </div>
+                    <div class="mt-10">
+                        <a href="https://blog.jpat.dev/" target="_blank">Blog</a>
+                    </div>
+                </div>
             </div>
         </div>
     </GuestLayout>
