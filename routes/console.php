6f8e769917a743c0ce3f82a55d5a147ffbf5d47f
Joao Patricio <ijpatricio@gmail.com>
2022-11-01 09:20:14 +0000

    Try demo compose

--- routes/console.php
+++ routes/console.php
@@ -15,5 +15,5 @@ use Illuminate\Support\Facades\Artisan;
 */
 
 Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
+    info(Inspiring::quote());
 })->purpose('Display an inspiring quote');
