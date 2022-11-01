daaa1ef69cb67c87a53f8beb94fceefcf1a74553
Joao Patricio <ijpatricio@gmail.com>
2022-11-01 07:42:24 +0000

    Try demo compose

--- resources/js/Pages/Results.vue
+++ resources/js/Pages/Results.vue
@@ -4,7 +4,8 @@ import {useVotingStore} from '@/Stores/useVotingStore'
 const voting = useVotingStore()
 import ApexCharts from 'apexcharts'
 import {onMounted, ref} from 'vue'
-
+import QrcodeVue from 'qrcode.vue'
+const link = window.location.href
 const chartEl = ref(null)
 
 onMounted(() => {
@@ -22,6 +23,11 @@ Echo
         <div class="mt-10 flex items-center justify-center text-2xl"> The results are in: </div>
         <div class="mt-8 flex items-center justify-center">
             <div ref="chartEl" id="chart"></div>
+        </div>
+
+        <div class=" w-full flex flex-col items-center">
+            <span class="text-xl"> Visit this page! </span>
+            <qrcode-vue background="#f3f4f6" :value="link" :size="300" level="H" />
         </div>
     </GuestLayout>
 </template>
