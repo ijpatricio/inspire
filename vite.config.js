ed8b49704ef05bb46e9afd40faa2a9b1ad4c94f4
Joao Patricio <ijpatricio@gmail.com>
2022-11-09 20:36:06 +0000

    Fix JS build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    build: {
+        target: 'esnext'
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
