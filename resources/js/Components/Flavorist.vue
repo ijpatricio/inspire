adc6cf085105d800a0b6f99a6596d602e3c6c857
Joao Patricio <ijpatricio@gmail.com>
2022-11-11 19:58:37 +0000

    Stars deploy with Traefik and SSL

---
+++ resources/js/Components/Flavorist.vue
@@ -0,0 +1,32 @@
+<template>
+    <slot v-bind="{ name, changeName }">
+        <div >
+            Fooo aaa
+        </div>
+    </slot>
+</template>
+
+<script>
+export default {
+
+    data: () => ({
+        name: 'Igerslike',
+    }),
+
+    methods: {
+        hello(msg) {
+            console.log('hello', msg)
+        },
+        changeName(newName) {
+            this.name = newName
+        },
+    },
+
+    render() {
+        return this.$scopedSlots.default({
+            name: this.name,
+            changeName: this.changeName,
+        })
+    },
+}
+</script>
